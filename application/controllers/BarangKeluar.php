c2687292d24e683bd7e6f1ef394c0e050f2646e4
fajar cahyadi putra <fajarcahyadiputra@gmail.com>
2021-01-23 08:22:03 +0700

    revisi report barang keluar

--- application/controllers/BarangKeluar.php
+++ application/controllers/BarangKeluar.php
@@ -2,36 +2,36 @@
 
 class BarangKeluar extends CI_Controller
 {
-	
+
 	function __construct()
 	{
 		parent::__construct();
-		$this->load->model('M_dataBarang','barang');
-		$this->load->model('M_barangKeluar','keluar');
+		$this->load->model('M_dataBarang', 'barang');
+		$this->load->model('M_barangKeluar', 'keluar');
 		$this->load->library('Pdf');
 	}
 	public function index()
 	{
 		$data = [
-			'barang'=> $this->barang->getBarang(),
-			'keluar'=>  $this->keluar->getKeluar(),
+			'barang' => $this->barang->getBarang(),
+			'keluar' =>  $this->keluar->getKeluar(),
 			'title' => 'Data Barang Keluar',
 		];
 		$this->load->view('admin/templet/header', $data);
 		$this->load->view('admin/templet/sidebar');
-		$this->load->view('admin/barangKeluar',$data);
+		$this->load->view('admin/barangKeluar', $data);
 		$this->load->view('admin/templet/footer');
 	}
 	public function tambahKeluar()
 	{
 		$id_barang = $this->input->post('id_barang');
 		$jumblah   = $this->input->post('jumblah');
-		$keterangan= $this->input->post('keterangan');
+		$keterangan = $this->input->post('keterangan');
 
 		$stok      = $this->barang->getBarang(['id' => $id_barang]);
 		$hasil_stok = $stok->stok - $jumblah;
 
-		if($hasil_stok >= 0){
+		if ($hasil_stok >= 0) {
 
 			$data 		= [
 				'id_barang'	=> $id_barang,
@@ -44,41 +44,41 @@ class BarangKeluar extends CI_Controller
 			$pesan = [];
 			$tambah = $this->keluar->tambahBarang($data);
 
-			if($tambah['tambah'] === true){
+			if ($tambah['tambah'] === true) {
 				$this->barang->editData(['stok' => $hasil_stok], ['id' => $id_barang]);
 				$pesan['id']     = $tambah['id'];
 				$pesan['tambah'] = true;
-			}else{
+			} else {
 				$pesan['tambah'] = false;
 			}
 
 			echo json_encode($pesan);
-
-		}else{
+		} else {
 			$pesan['tambah'] = 'kosong';
 			$pesan['stok']   = $stok->stok;
 			echo json_encode($pesan);
 		}
-
 	}
 	public function cetak_invoice($id)
 	{
-		$data    = [
-			'keluar'  => $this->keluar->getKeluar(['id' => $id]),
-			'title'   => 'Laporan Barang Keluar'
-		];
+		$data['keluar'] = $this->keluar->getKeluar(['id' => $id]);
+		$this->load->view('laporan_invoice', $data);
+		// $data    = [
+		// 	'keluar'  => $this->keluar->getKeluar(['id' => $id]),
+		// 	'title'   => 'Laporan Barang Keluar'
+		// ];
 
-		$this->pdf->filename = "laporan barang keluar.pdf";
-		$this->pdf->load_view('laporan_invoice', $data);
+		// $this->pdf->filename = "laporan barang keluar.pdf";
+		// $this->pdf->load_view('laporan_invoice', $data);
 	}
 	public function HapusKeluar()
 	{
 		$where = ['id' => $this->input->post('id')];
 		$hapus = $this->keluar->removeStufOut($where);
 		$pesan = [];
-		if($hapus){
+		if ($hapus) {
 			$pesan['hapus'] = true;
-		}else{
+		} else {
 			$pesan['hapus'] = false;
 		}
 
@@ -94,14 +94,14 @@ class BarangKeluar extends CI_Controller
 	{
 		$id_barang = $this->input->post('id_barang');
 		$jumblah   = $this->input->post('jumblah');
-		$keterangan= $this->input->post('keterangan');
+		$keterangan = $this->input->post('keterangan');
 		$tanggal_keluar = $this->input->post('tanggal_keluar');
 		$id_barang_lama = $this->input->post('id_barang_lama');
 
 		$stok      = $this->barang->getBarang(['id' => $id_barang]);
 		$hasil_stok = $stok->stok - $jumblah;
 
-		if($hasil_stok >= 0){
+		if ($hasil_stok >= 0) {
 
 			$data 		= [
 				'id_barang'	=> $id_barang,
@@ -116,16 +116,16 @@ class BarangKeluar extends CI_Controller
 			$pesan = [];
 			$edit = $this->keluar->editKeluar($data, $where);
 
-			if($edit['edit'] === true){
+			if ($edit['edit'] === true) {
 				$check = $this->keluar->getKeluar(['id' => $this->input->post('id')]);
-				if($check->jumblah != $jumblah){
+				if ($check->jumblah != $jumblah) {
 					$tambah_stok = $stok->stok + $check->jumblah;
 					$this->barang->editData(['stok' => $tambah_stok], ['id' => $id_barang_lama]);
 					$this->barang->editData(['stok' => $hasil_stok], ['id' => $id_barang]);
 				}
 				$pesan['id']     = $edit['id'];
 				$pesan['edit'] = true;
-			}else{
+			} else {
 				$pesan['edit'] = false;
 			}
 
--- application/views/laporan_invoice.php
+++ application/views/laporan_invoice.php
@@ -1,31 +1,44 @@
+<div style="display: flex">
+	<img style="margin-top: 15px; margin-right: 10px" width="150" height="100" src="<?php echo base_url('assets/img/logo_report.jpg') ?>">
+	<div style="display: inline-table; line-height: 5px; margin-top: 25px">
+		<p>JL.Raya Narogong Km.16,</p>
+		<p>Cileungsi Bogor 16820 - Indonesia</p>
+		<p>Tel : +62-21 8230 525;8249 1720</p>
+		<p>Fax : +62-21 8230 525;8249 1720</p>
+	</div>
+</div>
 
-<table>
-	<tr>
-		<th>Tanggal</th>
-		<td>:</td>
-		<td><?php echo date('Y-m-d') ?></td>
-	</tr>
-	<tr>
-		<th>Admin</th>
-		<td>:</td>
-		<td><?php echo $this->db->get_where('tb_users',['id' => $keluar->id_user])->row()->nama; ?></td>
-	</tr>
-</table>
-<hr>
-<table border="1" cellspacing="0" cellpadding="6" style="width: 100%">
-	<thead>	
+<div style="display:flex ; justify-content: flex-end; ">
+	<table>
 		<tr>
-			<th >id</th>
-			<th >Nama Barang</th>
-			<th >Jumblah</th>
-			<th >tanggal Keluar</th>
-			<th >Keterangan</th>
+			<th>Tanggal</th>
+			<td>:</td>
+			<td><?php echo date('Y-m-d') ?></td>
+		</tr>
+		<tr>
+			<th>Admin</th>
+			<td>:</td>
+			<td><?php echo $this->db->get_where('tb_users', ['id' => $keluar->id_user])->row()->nama; ?></td>
+		</tr>
+	</table>
+</div>
+<p>Nomer Surat Jalan : TR<?php echo time() ?></p>
+
+<hr>
+<table border="1" cellspacing="0" cellpadding="6" style="width: 100%; margin-top: 20px;">
+	<thead>
+		<tr>
+			<th>id</th>
+			<th>Nama Barang</th>
+			<th>Jumblah</th>
+			<th>tanggal Keluar</th>
+			<th>Keterangan</th>
 		</tr>
 	</thead>
 	<tbody>
 		<tr>
 			<td width="5%"><?php echo $keluar->id ?></td>
-			<td width="10%"><?php echo $this->db->get_where('tb_barang',['id' => $keluar->id_barang])->row()->nama_barang;?></td>
+			<td width="10%"><?php echo $this->db->get_where('tb_barang', ['id' => $keluar->id_barang])->row()->nama_barang; ?></td>
 			<td width="10%"><?php echo $keluar->jumblah ?></td>
 			<td width="10%"><?php echo $keluar->tanggal_keluar ?></td>
 			<td width="30%" style="word-break: break-all;"><?php echo $keluar->keterangan ?></td>
@@ -33,10 +46,23 @@
 	</tbody>
 </table>
 
-<div style="text-align: right; margin-top: 20px; margin-right: 50px">
-	<div style="margin-bottom: 70px">
+<!-- <div style="float: left; margin-top: 50px;">
+	<div style="margin-bottom: 90px">
+		Bekasi,...
+	</div> -->
+
+
+</div>
+
+<div style="text-align: right;  margin-top: 50px;">
+	<div style="margin-bottom: 90px">
 		Bekasi, <?php echo date('Y-m-d') ?>
 	</div>
 
-	<div style=""><?php echo $this->db->get_where('tb_users',['id' => $keluar->id_user])->row()->nama; ?></div>
+	<div style=""><?php echo $this->db->get_where('tb_users', ['id' => $keluar->id_user])->row()->nama; ?></div>
 </div>
+
+
+<script>
+	window.print();
+</script>
